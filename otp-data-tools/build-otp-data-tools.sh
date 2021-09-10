962d2a6f33ce2d6f74e883ff822b56d297d21b50
Joel Lappalainen <lappalj8@gmail.com>
2021-09-10 14:14:51 +0300

    Initial changes towards updating otp-data-tools to use python3

--- otp-data-tools/build-otp-data-tools.sh
+++ otp-data-tools/build-otp-data-tools.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 # this script is run inside the otp-data-tools container
-set +e
+set -e
 apt-get update && \
   apt-get -y install \
-    git build-essential python-dev protobuf-compiler libprotobuf-dev \
-    make swig g++ python-dev libreadosm-dev \
+    git build-essential python3-dev protobuf-compiler libprotobuf-dev \
+    make swig g++ libreadosm-dev \
     libboost-graph-dev libproj-dev libgoogle-perftools-dev \
-    osmctools unzip zip python-pyproj wget python-argh \
-    python-scipy python-sklearn python-pip python-numpy curl
+    osmctools unzip zip wget python3-pyproj python3-argh \
+    python3-scipy python3-sklearn python3-pip python3-numpy curl
 
 rm -rf /var/lib/apt/lists/*
 
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py && \
-  python get-pip.py && \
-  pip install imposm.parser && \
-  pip install argh && \
-  pip install future && \
-  pip install grequests && \
-  pip install unicodecsv && \
-  pip install cffi && \
-  pip install utm
+pip3 install git+https://github.com/lechup/imposm-parser@python3 && \
+  pip3 install argh && \
+  pip3 install future && \
+  pip3 install grequests && \
+  pip3 install unicodecsv && \
+  pip3 install cffi && \
+  pip3 install utm && \
+  pip3 install pyproj
 
 mkdir -p one-busaway-gtfs-transformer && \
   wget -O one-busaway-gtfs-transformer/onebusaway-gtfs-transformer-cli.jar "http://nexus.onebusaway.org/service/local/artifact/maven/content?r=public&g=org.onebusaway&a=onebusaway-gtfs-transformer-cli&v=1.3.9"
-
-git clone https://github.com/jswhit/pyproj.git
-cd pyproj
-git checkout ec9151e8c6909f7fac72bb2eab927ff18fa4cf1d
-python setup.py build
-python setup.py install
-cd ..
 
 git clone --recursive -b fastmapmatch https://github.com/HSLdevcom/gtfs_shape_mapfit.git
 cd gtfs_shape_mapfit
@@ -37,4 +29,3 @@ make -C pymapmatch
 cd ..
 
 git clone https://github.com/HSLdevcom/OTPQA.git
-
